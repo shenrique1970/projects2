5fafc1dd25f45992f7ca18e7f3be7910a12b3fbc
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2023-02-24 08:32:20 -0300

    Implementado busca e paginação usando sqlite3

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form, create, view
+from app.views import home, form, create, view, edit, update
+from app.views import delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,7 @@ urlpatterns = [
     path('form/', form, name='form'),
     path('create/', create, name='create'),
     path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>/', delete, name='delete'),
 ]
